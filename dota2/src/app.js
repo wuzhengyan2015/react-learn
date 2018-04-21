9aaff5287a6a8433f7f3cce7d56fa40db700d7a8
wzy2016 <306945468@qq.com>
2018-04-21 20:12:11 +0800

    change to dota2

---
+++ dota2/src/app.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+import 'bootstrap/dist/css/bootstrap.min.css';
+ 
+const App = () => <div>rimworld wiki23</div>
+ 
+export default hot(module)(App)
---
+++ dota2/webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: path.join(__dirname, './src/index.js'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: "index.js"
+    },
+    devtool: 'inline-source-map', 
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            use: 'babel-loader?cacheDirectory=true',
+            exclude: /node_modules/
+        }, {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }, {
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+            exclude: /node_modules/
+        }, {
+            test: /\.(jpg|png|gif)$/,
+            use: [{
+                loader: 'url-loader',
+                options: {
+                    limit: 8192
+                }
+            }]
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.join(__dirname, 'src/index.html')
+        })
+    ],
+    devServer: {
+        port: 8080,
+        historyApiFallback: true,
+        open: true,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8090',
+                pathRewrite: {'^/api' : ''},
+                changeOrigin: true
+              }
+        }
+    }
+}
