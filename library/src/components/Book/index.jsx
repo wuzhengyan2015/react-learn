90b27b295604a4f6979fe5a4d05856cb5387b70f
吴政衍 <160201@nd.com>
2018-06-02 14:39:42 +0800

    delete library folder

--- library/src/components/Book/index.jsx
+++
@@ -1,7 +0,0 @@
-import React, {Component} from 'react'
-
-export default function () {
-    return (
-        <div>书籍</div>
-    )
-}
--- library/src/components/Category/index.jsx
+++
@@ -1,7 +0,0 @@
-import React, {Component} from 'react'
-
-export default function () {
-    return (
-        <div>分类</div>
-    )
-}
