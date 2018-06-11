8b360737a6b62314c4aab1c104a0a660cc0146b1
wzy2016 <306945468@qq.com>
2018-06-12 07:00:09 +0800

    update

--- dota2/src/redux/reducers/versions.js
+++ dota2/src/redux/reducers/versions.js
@@ -6,11 +6,11 @@ import {
 
 const versions = (state = {}, action) => {
   switch (action.type) {
-    case GET_VERsIONS_PENDING:
+    case GET_VERSIONS_PENDING:
       return { isPending: true };
-    case GET_VERsIONS_FULFILLED:
+    case GET_VERSIONS_FULFILLED:
       return { body: action.payload };
-    case GET_VERsIONS_REJECTED:
+    case GET_VERSIONS_REJECTED:
       return { body: action.payload };
     default: 
       return state
