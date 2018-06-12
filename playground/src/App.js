969a77b07557a0d103137836f115b62cca229685
吴政衍 <160201@nd.com>
2018-06-12 08:21:28 +0800

    update: playgorund

--- playground/src/App.js
+++ playground/src/App.js
@@ -6,15 +6,51 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Parent />
+        <Parent2 />
       </div>
     );
+  }
+}
+
+class CustomTextInput extends React.Component {  
+  render () {
+    return (  
+      <div>  
+        <input ref={this.props.inputRef} value="12" readOnly/>  
+      </div>  
+    );
+  }
+    
+}  
+  
+class Parent extends React.Component {
+  render() {  
+    setTimeout(() => {
+      console.log(this.inputElement && this.inputElement.value)
+    }, 1500);
+    return (  
+      <CustomTextInput  
+        inputRef={el => this.inputElement = el}  
+      />  
+    );  
+  }  
+}  
+
+class Parent2 extends React.Component {
+  constructor () {
+    super()
+    this.formInput = React.createRef()
+  }
+  render() {  
+    setTimeout(() => {
+      console.log(this.formInput.current && this.formInput.current.value)
+    }, 1500);
+    return (  
+      <CustomTextInput  
+        inputRef={this.formInput}  
+      />   
+    )
   }
 }
 
