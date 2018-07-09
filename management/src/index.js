c997a8ea9fe1e448da88c7e2549435874552390e
wzy2016 <306945468@qq.com>
2018-07-09 21:53:11 +0800

    update management

--- management/src/index.js
+++ management/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './app';
-import './index.scss';
 
 ReactDom.render(
   <App />,
--- management/webpack.config.js
+++ management/webpack.config.js
@@ -21,6 +21,10 @@ module.exports = {
         use: ['babel-loader', 'eslint-loader'],
       },
       {
+        test: /\.(css)$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
         test: /\.(scss)$/,
         include: path.resolve(__dirname, 'src'),
         use: ['style-loader', 'css-loader', 'sass-loader'],
