ed2f952cf2f3a76a7ceecfd7044aa83ded8ad03b
wzy2016 <306945468@qq.com>
2018-07-14 10:28:52 +0800

    update playground

--- playground/src/App.js
+++ playground/src/App.js
@@ -9,10 +9,39 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <ReduxPromiseDemo />
+        <EnhanceDemo />
       </div>
     )
   }
 }
+
+function getDisplayName(component) {
+  return component.displayName || component.name || 'Component';
+}
+function withHeader(WrappedComponent) {
+  return class HOC extends Component {
+    static displayName = `HOC(${getDisplayName(WrappedComponent)})`
+    render() {
+      return <div>
+        <div className="demo-header">
+          我是标题
+        </div>
+        <WrappedComponent {...this.props}/>
+      </div>
+    }
+  }
+}
+
+class Demo extends Component {
+  render() {
+    return (
+      <div>
+        我是一个普通组件
+      </div>
+    );
+  }
+}
+
+const EnhanceDemo = withHeader(Demo);
 
 export default App;
