c00909c923859f54e76abe95b18e6ebba118182b
wzy2016 <306945468@qq.com>
2018-08-11 12:40:59 +0800

    update

---
+++ management/src/components/BreadCrumb/BreadCrumb.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+/* eslint-disable */
+class BreadCrumb extends Component {
+  render() {
+    return (
+      <div>
+        state
+      </div>
+    )
+  }
+}
+
+export default BreadCrumb
