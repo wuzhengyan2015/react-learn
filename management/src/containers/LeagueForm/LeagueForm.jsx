88dcd8e44ec7225f3fb49ba3e5dee2add8fb8471
吴政衍 <160201@nd.com>
2018-08-24 09:22:11 +0800

    update form

--- management/src/containers/LeagueForm/LeagueForm.jsx
+++ management/src/containers/LeagueForm/LeagueForm.jsx
@@ -2,31 +2,58 @@ import React, { Component } from 'react'
 import apiService from '../../services/index'
 import Table from '../../components/Table/Table'
 
-const dataSource = [{
-  key: '1',
-  name: '胡彦斌',
-  age: 32,
-  address: '西湖区湖底公园1号'
-}, {
-  key: '2',
-  name: '胡彦祖',
-  age: 42,
-  address: '西湖区湖底公园1号'
-}];
-
 const columns = [{
-  title: '姓名',
+  title: 'Name',
   dataIndex: 'name',
   key: 'name',
+  render: text => <a>{text}</a>,
 }, {
-  title: '年龄',
+  title: 'Age',
   dataIndex: 'age',
   key: 'age',
 }, {
-  title: '住址',
+  title: 'Address',
   dataIndex: 'address',
   key: 'address',
+}, {
+  title: 'Tags',
+  key: 'tags',
+  dataIndex: 'tags',
+  render: tags => (
+    <span>
+      {tags.map(tag => <span color="blue" key={tag}>{tag}</span>)}
+    </span>
+  ),
+}, {
+  title: 'Action',
+  key: 'action',
+  render: (text, record) => (
+    <span>
+      <a>Invite {record.name}</a>
+      <a>Delete</a>
+    </span>
+  ),
 }];
+
+const data = [{
+  key: '1',
+  name: 'John Brown',
+  age: 32,
+  address: 'New York No. 1 Lake Park',
+  tags: ['nice', 'developer'],
+}, {
+  key: '2',
+  name: 'Jim Green',
+  age: 42,
+  address: 'London No. 1 Lake Park',
+  tags: ['loser'],
+}, {
+  key: '3',
+  name: 'Joe Black',
+  age: 32,
+  address: 'Sidney No. 1 Lake Park',
+  tags: ['cool', 'teacher'],
+}]
 
 /* eslint-disable */
 class LeagueForm extends Component {
@@ -38,7 +65,7 @@ class LeagueForm extends Component {
   render() {
     return (
       <div className="league-form">
-        <Table dataSource={dataSource} columns={columns}/>
+        <Table dataSource={data} columns={columns}/>
       </div>
     )
   }
---
+++ management/src/utils/utils.js
@@ -0,0 +1,1 @@
+export const getType = obj => Object.prototype.toString.call(obj).slice(8, -1)
