e27a41a0f579aa2fccf1526dd5a81f03bec121f2
吴政衍 <160201@nd.com>
2018-09-29 10:00:58 +0800

    update 小调整

--- management/src/views/PlayerPage/PlayerPage.jsx
+++ management/src/views/PlayerPage/PlayerPage.jsx
@@ -14,7 +14,7 @@ class PlayerPage extends Component {
         <Tabs defaultActiveKey="lazyLoad" onChange={this.swtichPanel}>
           <TabPane tab="懒加载" key="lazyLoad">Content of Tab Pane 1</TabPane>
           <TabPane tab="滚动加载" key="scrollLoad">Content of Tab Pane 2</TabPane>
-        </Tabs>,
+        </Tabs>
       </div>
     )
   }
