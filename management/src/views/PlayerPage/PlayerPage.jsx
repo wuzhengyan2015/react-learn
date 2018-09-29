dd974b59acdf7de1e93d902ae37cfa4e8506571f
吴政衍 <160201@nd.com>
2018-09-29 10:00:08 +0800

    feat 球员管理页面基础

--- management/src/views/PlayerPage/PlayerPage.jsx
+++ management/src/views/PlayerPage/PlayerPage.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
+import { Tabs } from 'antd'
 
-/* eslint-disable */
+const TabPane = Tabs.TabPane
+
 class PlayerPage extends Component {
-  render () {
+  swtichPanel = (key) => {
+    console.log(key)
+  }
+
+  render() {
     return (
       <div>
-        PlayerPage
+        <Tabs defaultActiveKey="lazyLoad" onChange={this.swtichPanel}>
+          <TabPane tab="懒加载" key="lazyLoad">Content of Tab Pane 1</TabPane>
+          <TabPane tab="滚动加载" key="scrollLoad">Content of Tab Pane 2</TabPane>
+        </Tabs>,
       </div>
     )
   }
