826c0181a76afcd158d809b95fc7588b75ab7454
吴政衍 <160201@nd.com>
2018-10-27 14:00:07 +0800

    update

--- dota2/src/redux/store.js
+++ dota2/src/redux/store.js
@@ -1,11 +1,14 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import promiseMiddleware from 'redux-promise-middleware'
 import logger from 'redux-logger'
 import reducers from './reducers'
 
-const store = createStore(reducers, {}, applyMiddleware(
-    promiseMiddleware(),
-    logger
-))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, /* preloadedState, */ composeEnhancers(
+    applyMiddleware(
+        promiseMiddleware(),
+        logger
+    )
+));
 
 export default store
--- playground/src/App.js
+++ playground/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Grid />
+        <DemoWithHeader />
       </div>
     )
   }
@@ -25,10 +25,10 @@ class App extends Component {
 // })
 
 function getDisplayName(component) {
+  console.log(component.displayName, component.name)
   return component.displayName || component.name || 'Component';
 }
-function withHeader
-(WrappedComponent) {
+function withHeader(WrappedComponent) {
   return class HOC extends Component {
     static displayName = `HOC(${getDisplayName(WrappedComponent)})`
     render() {
@@ -41,6 +41,20 @@ function withHeader
     }
   }
 }
+function withHeader2 (WrapperComponent) {
+  return class HOC extends Component {
+      static displayName = `HOC${WrapperComponent.displayName || WrapperComponent.name || 'Component'}`
+      render() {
+          return (
+              <div>
+                  <h2>title</h2>
+                  <WrapperComponent {...this.props} />
+              </div>
+          )
+      }
+  }
+}
+
 
 class Demo extends Component {
   render() {
@@ -51,6 +65,7 @@ class Demo extends Component {
     );
   }
 }
+const DemoWithHeader = withHeader2(Demo)
 
 const EnhanceDemo = withHeader(Demo);
 
